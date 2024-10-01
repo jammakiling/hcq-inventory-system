5b4b084c2fb27a9a810ffa77a332d9023073a14f
jammakiling <154589125+jammakiling@users.noreply.github.com>
2024-10-01 21:25:15 +0800

    Built CRUD pages for product list basics

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'inventory'
+
 urlpatterns = [
-    path('product-list', views.product_list),
+    path('products/', views.product_list ,name ='product_list'),
+    path('products/create/', views.product_create, name='product_create'),
+    path('products/update/<int:pk>/', views.product_update, name='product_update'),
+    path('products/delete/<int:pk>/', views.product_delete, name='product_delete'),
 
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from . import forms
+from .models import Product
+from .forms import ProductForm
 
 
 # Create your views here.
 def product_list(request):
-   return render(request, 'inventory/product_list.html')
+   products = Product.objects.all()
+   return render(request, 'inventory/product_list.html' , {'products': products})
+
+def product_create(request):
+   if request.method == 'POST':
+      form = ProductForm(request.POST)
+      if form.is_valid():
+         form.save()
+         return redirect('inventory:product_list')
+   else:
+      form = ProductForm()
+      return render (request, 'inventory/product_form.html' , {'form' :form})
+   
+def product_update(request, pk):
+   product = get_object_or_404(Product, pk=pk)
+   if request.method =='POST':
+      form =ProductForm(request.POST, instance=product)
+      if form.is_valid():
+         form.save()
+         return redirect('inventory:product_list')
+   else:
+      form = ProductForm(instance=product)
+   return render(request, 'inventory/product_form.html', {'form': form})
+
+def product_delete(request, pk):
+   product = get_object_or_404(Product, pk=pk)
+   if request.method == 'POST':
+      product.delete()
+      return redirect('inventory:product_list')
+   return render(request, 'inventory/product_confirm_delete.html', {'product':product})
